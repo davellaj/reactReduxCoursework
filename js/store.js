6631afd67fc1da8e3a6670ded6e3ba56ad98564b
davellaj <jamie.davella@gmail.com>
2016-12-15 01:22:10 -0500

    add fetch call to actions as well as new reducers

--- js/store.js
+++ js/store.js
@@ -1,5 +1,10 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import * as reducers from './reducers/index';
 
-export default createStore(reducers.repositoryReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore(reducers.repositoryReducer, applyMiddleware(thunk));
+
+// export default createStore(reducers.repositoryReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
